1bf55874a1762eae6028eca35a0e9e5d5a678a83
Camilo Sanchez <daniel.sanchez24102001@gmail.com>
2020-11-20 19:38:40 -0500

    Corrigiendo ruta de rememberTask

--- src/pages/TASKS/RememberPage.jsx
+++ src/pages/TASKS/RememberPage.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import RememberTask from "../../components/rememberTask/RememberTask";
+import RememberTask from '../../components/rememberTask/RememberTask';
 import "../../styles/style.css";
 import "../../styles/rememberOrderTask.css";
 import Back from "../../img/fontReactor.png";
