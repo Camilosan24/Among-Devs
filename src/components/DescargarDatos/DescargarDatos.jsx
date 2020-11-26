4467e7d4035bbca82525b123d3d187c82a8bc941
edwincasallas22 <casallas22@hotmail.com>
2020-11-26 17:16:27 -0500

    Se agrego Js y html

--- src/components/DescargarDatos/DescargarDatos.jsx
+++ src/components/DescargarDatos/DescargarDatos.jsx
@@ -7,19 +7,20 @@ class DescargarDatos extends React.Component {
   constructor(props) {
     super(props);
     this.canvas = React.createRef();
+    this.LIMITE_CARGA = 355;
+    this.interval = null;
 
+    this.rect = {
+      posX: 85,
+      posY: 240,
+      ancho: 10,
+      alto: 30,
+    };
   }
   state = {
     startGif: pantallazo,
-    //   this.LIMITE_CARGA: 355,
-    //   rect: {
-    //     posX: 85,
-    // //     posY: 240,
-    // //     ancho: 10,  
-    // //     alto: 30,
-    // //   },
-    //   interval: null,
   };
+
 
   componentDidMount() {
     this.drawRectInCanvas();
@@ -31,6 +32,58 @@ class DescargarDatos extends React.Component {
     ctx.fillStyle = "rgb(34, 143, 25)";
   };
 
+
+
+
+
+
+  // pintarImagenDeFondo();
+
+
+
+  moverBarra = () => {
+    if (this.interval === null) {
+      this.interval = this.setInterval(function () {
+        if (this.estaEnElLimite()) {
+          alert("Tarea completada");
+          this.gif.src = "./pantallazo.png";
+          clearInterval(this.interval);
+          this.interval = null;
+        }
+
+        this.rect.ancho += 7;
+        this.limpiarTablero();
+        this.pintarRectangulo(this.rect);
+        this.pintarImagenDeFondo();
+      }, 100);
+
+    } else {
+      clearInterval(this.interval);
+      this.interval = null;
+    }
+  }
+
+  estaEnElLimite = () => {
+
+    return this.rect.ancho >= this.LIMITE_CARGA;
+  }
+  limpiarTablero = () => {
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+  }
+  pintarRectangulo = ({ posX, posY, ancho, alto }) => {
+    this.ctx.fillRect(posX, posY, ancho, alto);
+  }
+  pintarImagenDeFondo = () => {
+    this.ctx.drawImage(this.imagenBarra, 0, 0, this.canvas.width, this.canvas.height);
+  }
+
+
+
+
+
+
+
+
   render() {
     return (
       <React.Fragment>
@@ -39,14 +92,17 @@ class DescargarDatos extends React.Component {
         <button
           id="butDow"
           onClick={() =>
+
             this.setState({
               startGif:
                 "https://media.giphy.com/media/tJrjQ5HzlOStHInsmw/giphy.gif",
-            })
+            }), this.moverBarra
+
+
           }
         >
           Download
-        </button>
+      </button>
         <main>
           <div class="carga">
             <img
@@ -67,9 +123,10 @@ class DescargarDatos extends React.Component {
             />
           </div>
         </main>
+        <canvas id="game" ref={this.canvas} height="400" width="600"></canvas>
+        <br />
       </React.Fragment>
     );
   }
 }
-
 export default DescargarDatos;
